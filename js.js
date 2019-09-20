f983c574305925dc2d77769a53ef2cba8957f518
Fahad <fahad011686@gmail.com>
2019-09-20 17:58:48 -0500

    Fixed upload to Firebase and pulling data from it.

--- js.js
+++ js.js
@@ -1,16 +1,16 @@
-// var firebaseConfig = {
-//     apiKey: "AIzaSyAlrVf4F7JZQULpHDpNM5jlKyQjoZi5zzw",
-//     authDomain: "project-1-66f61.firebaseapp.com",
-//     databaseURL: "https://project-1-66f61.firebaseio.com",
-//     projectId: "project-1-66f61",
-//     storageBucket: "",
-//     messagingSenderId: "119880972700",
-//     appId: "1:119880972700:web:beb7c5dcd990a02786f059"
-//   };
-//   firebase.initializeApp(firebaseConfig);
+var firebaseConfig = {
+    apiKey: "AIzaSyAlrVf4F7JZQULpHDpNM5jlKyQjoZi5zzw",
+    authDomain: "project-1-66f61.firebaseapp.com",
+    databaseURL: "https://project-1-66f61.firebaseio.com",
+    projectId: "project-1-66f61",
+    storageBucket: "",
+    messagingSenderId: "119880972700",
+    appId: "1:119880972700:web:beb7c5dcd990a02786f059"
+  };
+  firebase.initializeApp(firebaseConfig);
 //   firebase.initializeApp(config);
   
-//   var database = firebase.database();
+  var database = firebase.database();
   
   $("#add-train-btn").on("click", function(event) {
     event.preventDefault();
